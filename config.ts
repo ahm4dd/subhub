b78a44fbb034241aae6a20f882e33700607493c5
ahm4dd <ahmed.emad.edu@gmail.com>
2025-07-18 17:01:26 +0300

    feat(routes): add base routes

--- config.ts
+++ config.ts
@@ -3,10 +3,10 @@ process.loadEnvFile(`.env.${process.env.NODE_ENV || "development"}.local`);
 
 type ServerConfig = {
   HOSTNAME: string;
-  PORT;
+  PORT: number;
 };
 
 export const serverConfig: ServerConfig = {
   HOSTNAME: process.env.HOSTNAME || "127.0.0.1",
-  PORT: process.env.PORT || 3000,
+  PORT: (process.env.PORT as unknown as number) || 3000,
 };
