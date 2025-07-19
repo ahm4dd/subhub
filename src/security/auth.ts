aabb24f261837922a2c59d9fe0c515ff5f0b2dd7
ahm4dd <ahmed.emad.edu@gmail.com>
2025-07-19 19:21:27 +0300

    feat(auth): added a way to hash passwords and compare them

--- src/security/auth.ts
+++ src/security/auth.ts
@@ -8,7 +8,7 @@ export function makeJWT(
   userId: string,
   secret: string,
   expiresIn: number = 3600,
-) {
+): string {
   type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
   const payload: Payload = {
     iss: "subhub",
@@ -17,4 +17,18 @@ export function makeJWT(
     exp: Math.floor(Date.now() / 1000) + expiresIn,
   };
   return jwt.sign(payload, secret);
+}
+
+export async function hashPassword(
+  password: string,
+  salt_rounds: number = SALT_ROUNDS,
+): Promise<string> {
+  return await bcrypt.hash(password, salt_rounds);
+}
+
+export async function checkPassword(
+  password: string,
+  hashedPassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(password, hashedPassword);
 }
