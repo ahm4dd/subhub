19e001c90d137265cc639093735013720168b0d4
ahm4dd <ahmed.emad.edu@gmail.com>
2025-07-20 20:08:14 +0300

    feat(db): added revoke get delete refresh token queries

--- src/db/queries/refreshTokens.ts
+++ src/db/queries/refreshTokens.ts
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import { db } from "../index.ts";
 import { RefreshToken, NewRefreshToken, refreshTokens } from "../schema.ts";
 
@@ -10,4 +11,26 @@ export async function createRefreshToken(
     .onConflictDoNothing()
     .returning();
   return token;
+}
+
+export async function getRefreshTokenByToken(token: string): Promise<RefreshToken> {
+  const [refreshToken] = await db.select().from(refreshTokens).where(eq(refreshTokens.token, token));
+  return refreshToken;
+}
+
+export async function revokeRefreshToken(token: string): Promise<RefreshToken> {
+  const [refreshToken] = await db
+    .update(refreshTokens)
+    .set({revokedAt: new Date()})
+    .where(eq(refreshTokens.token, token))
+    .returning();
+  return refreshToken;
+}
+
+export async function deleteRefreshToken(token: string): Promise<RefreshToken> {
+  const [refreshToken] = await db
+    .delete(refreshTokens)
+    .where(eq(refreshTokens.token, token))
+    .returning();
+  return refreshToken;
 }
