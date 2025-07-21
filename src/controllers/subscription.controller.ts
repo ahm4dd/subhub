50732aec601c2f22ff5bbc5d2b033237f15240ea
ahm4dd <ahmed.emad.edu@gmail.com>
2025-07-21 17:21:26 +0300

    feat(subscription): added an untested create subscription handler

---
+++ src/controllers/subscription.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import { createSubscription } from "../db/queries/subscriptions.ts";
+import { Subscription, NewSubscription } from "../db/schema.ts";
+import { AuthorizationError, BadRequestError, NotFoundError, DatabaseError, ServerError} from "../errors.ts";
+import { getUserById } from "../db/queries/users.ts";
+import { extractBearerToken, verifyJWT } from "../security/auth.ts";
+import { serverConfig } from "../config.ts";
+
+export async function createSubscriptionHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        if (req.body.name && req.body.price && req.body.category && req.body.startDate && req.body.userId && req.body.paymentMethod) {
+
+            const token: string | undefined = extractBearerToken(req);
+            if (!token) {
+                throw new BadRequestError("No token provided!");
+            }
+            const userId = verifyJWT(token, serverConfig.JWT_SECRET);
+            
+            if (req.body.userId !== userId) {
+                throw new AuthorizationError("Not authorized to create subscription!");
+            }
+            
+            const user = await getUserById(userId);
+            if (!user) {
+                throw new NotFoundError("User not found!");
+            }
+            
+            const subscription: Omit<NewSubscription, "renewalDate" | "status"> & { renewalDate?: Date } = req.body;
+            const newSubscription: Subscription = await createSubscription(subscription);
+            
+            if (!newSubscription) {
+                throw new ServerError("Could not create subscription!");
+            }
+            
+            res.status(200).json(newSubscription);
+        } else {
+            throw new BadRequestError("Invalid subscription data!");
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
